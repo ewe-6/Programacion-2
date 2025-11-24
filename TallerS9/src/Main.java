e4c3f971edd332ed1ff0cede22f730a2e151f22f
Isaac <isaacarturov13@gmail.com>
2025-11-24 10:29:40 -0500

    Add files via upload

--- TallerS9/src/Main.java
+++ TallerS9/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -24,25 +25,65 @@ public class Main {
 
                 switch(opcion){
                     case 1:
-                        ingresarProductoporPartes(espacioAlmacen);
+                        ingresarProductoporPartes(espacioAlmacen, saldo);
+                        if(!listaProductoporPartes.isEmpty()) {
+                            espacioAlmacen -= (listaProductoporPartes.getLast().getEspacioOcupa() * listaProductoporPartes.getLast().getCantidad());
+                            saldo -= (listaProductoporPartes.getLast().getCosto() * listaProductoporPartes.getLast().getCantidad());
+                        }
+                        break;
                     case 2:
-                        ingresarProductoensamblado(espacioAlmacen);
+                        ingresarProductoensamblado(espacioAlmacen, saldo);
+                        if(!listaProductoensamblado.isEmpty()) {
+                            espacioAlmacen -= (listaProductoensamblado.getLast().getEspacioOcupa() * listaProductoensamblado.getLast().getCantidad());
+                            saldo -= (listaProductoensamblado.getLast().getCosto() * listaProductoensamblado.getLast().getCantidad());
+                        }
+                        break;
                     case 3:
                         System.out.print("Ingresar la cantidad a depositar: ");
                         saldo += sc.nextDouble();
+                        break;
                     case 4:
                         System.out.print("Ingresar el espacio a aumentar: ");
                         espacioAlmacen += sc.nextInt();
+                        break;
+                    case 5:
+                        venderProductopartes();
+                        break;
+                    case 6:
+                        venderProductoensamblado();
+                        break;
                     case 7:
-                        mostrarProductos();
+                        aumentarProductopartes();
+                        break;
                     case 8:
+                        aumentarProductoensamblado();
+                        break;
+                    case 9:
+                        eliminarProductopartes();
+                        break;
+                    case 10:
+                        eliminarProductoensamblado();
+                        break;
+                    case 11:
+                        mostrarProductos();
+                        break;
+                    case 12:
+                        System.out.println("\n------------ DATOS DE LA EMPRESA ------------");
+                        System.out.println("Saldo: " + saldo);
+                        System.out.println("Espacio en almacén: " + espacioAlmacen);
+                    case 13:
                         System.out.println("Saliendo de la aplicación");
                         break;
                     default:
                         System.out.println("Se ingresó un número no válido");
+                        break;
                 }
+            }else {
+                System.out.println("\nEntrada no válida. Por favor, ingrese un número.");
+                sc.nextLine(); // Consumir la entrada no válida
+                opcion = 0; // Para que el bucle continúe
             }
-        } while (opcion!=8);
+        } while (opcion!=13);
     }
 
     private static void mostrarMenu(){
@@ -51,13 +92,19 @@ public class Main {
         System.out.println("2. Ingresar un nuevo producto ensamblado");
         System.out.println("3. Aumentar el saldo de la empresa");
         System.out.println("4. Aumentar el espacio en el almacén");
-        System.out.println("5. Venta de un producto");
-        System.out.println("6. Reabastecimiento de un producto");
-        System.out.println("7. Mostrar todos los productos");
-        System.out.println("8. Salir");
+        System.out.println("5. Venta de un producto por partes");
+        System.out.println("6. Venta de un producto ensamblado");
+        System.out.println("7. Reabastecimiento de un producto por partes");
+        System.out.println("8. Reabastecimiento de un producto ensamblado");
+        System.out.println("9. Eliminar un producto por partes");
+        System.out.println("10. Eliminar un producto ensamblado");
+        System.out.println("11. Mostrar todos los productos");
+        System.out.println("12. Mostrar el saldo de la empresa y espacio en almacén");
+        System.out.println("13. Salir");
+        System.out.print("Ingresar la operación a realizar: ");
     }
 
-    private static void ingresarProductoporPartes(int espacioAl){
+    private static void ingresarProductoporPartes(int espacioAl, double saldo){
         System.out.println("----------- Ingreso de Producto por Partes -----------");
         System.out.print("Producto: ");
         String nombre = sc.nextLine();
@@ -71,19 +118,25 @@ public class Main {
         int espacioOcupa = sc.nextInt();
         System.out.print("Espacio extra: ");
         int espacioExtra = sc.nextInt();
+        sc.nextLine();
 
         int espacioTot = espacioExtra+espacioOcupa;
         if ((espacioTot*cantidad)>espacioAl){
             System.out.println("La cantidad que intentó ingresar supera el espacio disponible en almacén");
             System.out.println("Saliendo...");
             return;
+        } else if ((cantidad*costo)>saldo){
+            System.out.println("La cantidad que intentó ingresar supera el saldo disponible");
+            System.out.println("Saliendo...");
+            return;
+        } else {
+            ProductoporPartes nuevoProducto = new ProductoporPartes(nombre, costo, precio, cantidad, espacioOcupa, espacioExtra);
+            listaProductoporPartes.add(nuevoProducto);
+            System.out.println("\nSe ha agregado con éxito el producto por partes");
         }
-        ProductoporPartes nuevoProducto = new ProductoporPartes(nombre, costo, precio, cantidad, espacioOcupa, espacioExtra);
-        listaProductoporPartes.add(nuevoProducto);
-        System.out.println("\nSe ha agregado con éxito el producto por partes");
     }
 
-    private static void ingresarProductoensamblado(int espacioAl){
+    private static void ingresarProductoensamblado(int espacioAl, double saldo){
         System.out.println("----------- Ingreso de Producto Ensamblado -----------");
         System.out.print("Producto: ");
         String nombre = sc.nextLine();
@@ -93,20 +146,26 @@ public class Main {
         double precio = sc.nextDouble();
         System.out.print("Cantidad: ");
         int cantidad = sc.nextInt();
-        System.out.print("Espacio que ocupa: ");
-        int espacioOcupa = sc.nextInt();
+        sc.nextLine();
         System.out.print("Tipo de ensamblaje: ");
         String tipoEnsamblaje = sc.nextLine();
+        System.out.print("Espacio que ocupa: ");
+        int espacioOcupa = sc.nextInt();
+        sc.nextLine();
 
         if ((espacioOcupa*cantidad)>espacioAl){
             System.out.println("La cantidad que intentó ingresar supera el espacio disponible en almacén");
             System.out.println("Saliendo...");
             return;
+        } else if ((cantidad*costo)>saldo){
+            System.out.println("La cantidad que intentó ingresar supera el saldo disponible");
+            System.out.println("Saliendo...");
+            return;
+        } else {
+            Productoensamblado nuevoProducto = new Productoensamblado(nombre, costo, precio, cantidad, espacioOcupa, tipoEnsamblaje);
+            listaProductoensamblado.add(nuevoProducto);
+            System.out.println("\nSe ha agregado con éxito el producto ensamblado");
         }
-
-        Productoensamblado nuevoProducto = new Productoensamblado(nombre, costo, precio, cantidad, espacioOcupa, tipoEnsamblaje);
-        listaProductoensamblado.add(nuevoProducto);
-        System.out.println("\nSe ha agregado con éxito el producto ensamblado");
     }
 
     private static void eliminarProductopartes(){
@@ -117,10 +176,11 @@ public class Main {
 
         System.out.print("Ingrese el nombre del producto a eliminar: ");
         String productoEliminar = sc.nextLine();
+        sc.nextLine();
 
         boolean encontrado = false;
         for (int i = 0; i < listaProductoporPartes.size(); i++) {
-            if (listaProductoporPartes.get(i).getNombre() == productoEliminar) {
+            if (Objects.equals(listaProductoporPartes.get(i).getNombre(), productoEliminar)) {
                 listaProductoporPartes.remove(i);
                 encontrado = true;
                 System.out.println("\nEl producto " + productoEliminar + " fue eliminado con éxito");
@@ -141,10 +201,11 @@ public class Main {
 
         System.out.print("Ingrese el nombre del producto a eliminar: ");
         String productoEliminar = sc.nextLine();
+        sc.nextLine();
 
         boolean encontrado = false;
         for (int i = 0; i < listaProductoensamblado.size(); i++) {
-            if (listaProductoensamblado.get(i).getNombre() == productoEliminar) {
+            if (Objects.equals(listaProductoensamblado.get(i).getNombre(), productoEliminar)) {
                 listaProductoensamblado.remove(i);
                 encontrado = true;
                 System.out.println("\nEl producto " + productoEliminar + " fue eliminado con éxito");
@@ -191,22 +252,116 @@ public class Main {
         }
 
         System.out.print("Ingrese el nombre del producto a reabastecer: ");
-        String productoEliminar = sc.nextLine();
+        String productoAumentar = sc.nextLine();
+        sc.nextLine();
 
         boolean encontrado = false;
         for (int i = 0; i < listaProductoporPartes.size(); i++) {
-            if (listaProductoporPartes.get(i).getNombre() == productoEliminar) {
+            if (Objects.equals(listaProductoporPartes.get(i).getNombre(), productoAumentar)) {
                 System.out.print("Ingrese la cantidad a aumentar: ");
                 int cant=sc.nextInt();
-                listaProductoporPartes.set(i, listaProductoporPartes.get(i).getCantidad()+cant);
+
+                listaProductoporPartes.get(i).setCantidad(listaProductoporPartes.get(i).getCantidad()+cant);
                 encontrado = true;
-                System.out.println("\nEl producto " + productoEliminar + " fue eliminado con éxito");
+                System.out.println("\nEl producto " + productoAumentar + " fue actualizado con éxito");
                 break;
             }
         }
 
         if (!encontrado) {
-            System.out.println("\n No se encontró el producto " + productoEliminar);
+            System.out.println("\n No se encontró el producto " + productoAumentar);
+        }
+    }
+    private static void aumentarProductoensamblado(){
+        if(listaProductoensamblado.isEmpty()){
+            System.out.println("No hay productos ensamblados para reabastecer");
+            return;
+        }
+
+        System.out.print("Ingrese el nombre del producto a reabastecer: ");
+        String productoAumentar = sc.nextLine();
+        sc.nextLine();
+
+        boolean encontrado = false;
+        for (int i = 0; i < listaProductoensamblado.size(); i++) {
+            if (Objects.equals(listaProductoensamblado.get(i).getNombre(), productoAumentar)) {
+                System.out.print("Ingrese la cantidad a aumentar: ");
+                int cant=sc.nextInt();
+                listaProductoensamblado.get(i).setCantidad(listaProductoensamblado.get(i).getCantidad()+cant);
+                encontrado = true;
+                System.out.println("\nEl producto " + productoAumentar + " fue actualizado con éxito");
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("\n No se encontró el producto " + productoAumentar);
+        }
+    }
+
+    private static void venderProductopartes(){
+        if(listaProductoporPartes.isEmpty()){
+            System.out.println("No hay productos por partes para vender");
+            return;
+        }
+
+        System.out.print("Ingrese el nombre del producto a vender: ");
+        String productoVender = sc.nextLine();
+        sc.nextLine();
+
+        boolean encontrado = false;
+        for (int i = 0; i < listaProductoporPartes.size(); i++) {
+            if (Objects.equals(listaProductoporPartes.get(i).getNombre(), productoVender)) {
+                System.out.print("Ingrese la cantidad a vender: ");
+                int cant=sc.nextInt();
+                if(cant>listaProductoporPartes.get(i).getCantidad()){
+                    System.out.println("Se está intentando vender una cantidad mayor a la que se tiene en inventario");
+                    System.out.println("Saliendo...");
+                    return;
+                }else {
+                    listaProductoporPartes.get(i).setCantidad(listaProductoporPartes.get(i).getCantidad() - cant);
+                    encontrado = true;
+                    System.out.println("\nEl producto " + productoVender + " fue vendido con éxito");
+                    break;
+                }
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("\n No se encontró el producto " + productoVender);
+        }
+    }
+
+    private static void venderProductoensamblado(){
+        if(listaProductoensamblado.isEmpty()){
+            System.out.println("No hay productos ensamblados para vender");
+            return;
+        }
+
+        System.out.print("Ingrese el nombre del producto a vender: ");
+        String productoVender = sc.nextLine();
+        sc.nextLine();
+
+        boolean encontrado = false;
+        for (int i = 0; i < listaProductoensamblado.size(); i++) {
+            if (Objects.equals(listaProductoensamblado.get(i).getNombre(), productoVender)) {
+                System.out.print("Ingrese la cantidad a vender: ");
+                int cant=sc.nextInt();
+                if(cant>listaProductoensamblado.get(i).getCantidad()){
+                    System.out.println("Se está intentando vender una cantidad mayor a la que se tiene en inventario");
+                    System.out.println("Saliendo...");
+                    return;
+                }else {
+                    listaProductoensamblado.get(i).setCantidad(listaProductoensamblado.get(i).getCantidad() - cant);
+                    encontrado = true;
+                    System.out.println("\nEl producto " + productoVender + " fue vendido con éxito");
+                    break;
+                }
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("\n No se encontró el producto " + productoVender);
         }
     }
 }
--- TallerS9/src/Producto.java
+++ TallerS9/src/Producto.java
@@ -16,21 +16,36 @@ public class Producto {
     public String getNombre() {
         return nombre;
     }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
     public double getCosto() {
         return costo;
+    }
+    public void setCosto(double costo) {
+        this.costo = costo;
     }
 
     public double getPrecio() {
         return precio;
     }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 
     public int getCantidad() {
         return cantidad;
     }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
 
     public int getEspacioOcupa() {
         return espacioOcupa;
+    }
+    public void setEspacioOcupa(int espacioOcupa) {
+        this.espacioOcupa = espacioOcupa;
     }
 
     public void mostrarInfo(){
